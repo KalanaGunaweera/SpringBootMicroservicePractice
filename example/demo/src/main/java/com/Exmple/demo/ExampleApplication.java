b659af5101504ddb23f9e63d442ded3f8e9ffc40
Kalana Gunaweera <kalanagunaweera94@gmail.com>
2024-09-24 10:48:10 +0530

    Model Mapper - Initialized in main class

--- example/demo/src/main/java/com/Exmple/demo/ExampleApplication.java
+++ example/demo/src/main/java/com/Exmple/demo/ExampleApplication.java
@@ -1,7 +1,9 @@
 package com.Exmple.demo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExampleApplication {
@@ -9,5 +11,10 @@ public class ExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
+	@Bean  //In Java, the @Bean annotation is used in the Spring Framework to define a method that produces a bean, which is a managed object in the Spring container.
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
 
 }
