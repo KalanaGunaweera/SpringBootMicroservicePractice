e083c34197daeac6c501d028abe70e7578db3e80
Kalana Gunaweera <kalanagunaweera94@gmail.com>
2024-09-24 11:34:09 +0530

    getUser api

--- example/demo/src/main/java/com/Exmple/demo/controller/UserController.java
+++ example/demo/src/main/java/com/Exmple/demo/controller/UserController.java
@@ -1,16 +1,25 @@
 package com.Exmple.demo.controller;
 
+import com.Exmple.demo.dto.UserDto;
+import com.Exmple.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/getUser")
-    public String getUser() {
-        return "All Users";   // http://localhost:8080/api/v1/getUser
+    public List<UserDto> getUser() {
+        return userService.getAllUsers();   // http://localhost:8080/api/v1/getUser
     }
+
 }
